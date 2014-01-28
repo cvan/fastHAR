c33f3f0c09ab61dc929faff32568f8081f4bb686
Christopher Van <cvan@mozilla.com>
2014-01-28 11:46:10 -0800

    bump

--- src/api_urls.js
+++ src/api_urls.js
@@ -1,5 +1,5 @@
 // No trailing slashes, please./
 var api_urls = {
     chromehar: 'http://cvan.github.io/chromeHAR',
-    fasthar: 'http://107.170.253.47:49174'
+    fasthar: 'http://107.170.253.47:49176'
 };
