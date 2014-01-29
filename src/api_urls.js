fa6db387711e772c9dda8dded2e8eb891150a9a0
Christopher Van <cvan@mozilla.com>
2014-01-29 14:11:16 -0800

    add api_urls

--- src/api_urls.js
+++ src/api_urls.js
@@ -1,5 +1,5 @@
 // No trailing slashes, please./
 var api_urls = {
     chromehar: 'http://cvan.github.io/chromeHAR',
-    fasthar: 'http://107.170.253.47:49176'
+    fasthar: 'http://fasthar-api.paas.allizom.org'
 };
